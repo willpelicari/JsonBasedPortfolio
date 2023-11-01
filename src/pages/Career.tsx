be91e06ef47cfcdfc583596ddbcdb716045900d8
Wilton Pelicari <willpelicari@gmail.com>
2023-10-31 20:33:09 -0400

    Renaming Career file name

--- src/pages/Career.tsx
+++ src/pages/Career.tsx
@@ -13,7 +13,7 @@ export function Career({ experiences }: CareerProps) {
   const filterContext = useContext(FilterContext)
 
   return (
-    <Section id="experiences" title={experiences.Title}>
+    <Section id="experiences" title={experiences.title}>
       <div className="mt-10 mx-auto text-black dark:text-white">
         <div className="flex justify-between mx-2 my-4">
           {filterContext.filteredTags.length > 0 && (
@@ -27,8 +27,8 @@ export function Career({ experiences }: CareerProps) {
           {/* <p>Showing {} out of {experiences.Entries.length}</p> */}
         </div>
         <ul className="space-y-12">
-          {experiences.Entries &&
-            experiences.Entries.map((entry, key) => (
+          {experiences.entries &&
+            experiences.entries.map((entry, key) => (
               <ExperienceEntry key={key} entry={entry} />
             ))}
         </ul>
